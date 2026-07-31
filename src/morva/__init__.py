2c790d1dd7eef9cc1f40a5e235994c6d549c4544
Ali-Marandi <Marandi@outlook.com>
2026-07-31 04:21:21 +0200

    chore: add src/morva/__init__.py

---
+++ src/morva/__init__.py
@@ -0,0 +1,6 @@
+"""Preliminary analysis tools for simply supported beams."""
+
+from .beam import Beam, PointLoad, Result, UDL
+
+__all__ = ["Beam", "PointLoad", "Result", "UDL"]
+__version__ = "0.1.0"
