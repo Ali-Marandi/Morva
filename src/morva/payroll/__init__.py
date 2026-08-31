986746af5560811a87fcfb4aa23ee3fb9752a303
‪Ali Marandi‬‏ <Marandi@outlook.com>
2026-09-01 01:32:50 +0200

    refactor(payroll): expose calculator policies and retro engine

--- src/morva/payroll/__init__.py
+++ src/morva/payroll/__init__.py
@@ -1,4 +1,20 @@
+from .calculator import PayrollCalculation, PayrollCalculator
 from .models import PayrollLine, PayrollResult
+from .policies import ContributionPolicy, TaxBracket, TaxPolicy, demo_iranian_policy_pack
+from .retro import RetroPeriod, RetroResult, calculate_retroactive
 from .service import PayrollService
 
-__all__ = ["PayrollLine", "PayrollResult", "PayrollService"]
+__all__ = [
+    "ContributionPolicy",
+    "PayrollCalculation",
+    "PayrollCalculator",
+    "PayrollLine",
+    "PayrollResult",
+    "PayrollService",
+    "RetroPeriod",
+    "RetroResult",
+    "TaxBracket",
+    "TaxPolicy",
+    "calculate_retroactive",
+    "demo_iranian_policy_pack",
+]
