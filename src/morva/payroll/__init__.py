1f49cff3c9b66124a816a18c7c2beecbad2d51e1
‪Ali Marandi‬‏ <Marandi@outlook.com>
2026-09-01 03:39:02 +0200

    feat: expose payroll snapshot and reconciliation APIs

--- src/morva/payroll/__init__.py
+++ src/morva/payroll/__init__.py
@@ -1,20 +1,43 @@
 from .calculator import PayrollCalculation, PayrollCalculator
+from .diff import EmployeeDiff, LineDiff, compare_snapshots, population_component_totals
 from .models import PayrollLine, PayrollResult
 from .policies import ContributionPolicy, TaxBracket, TaxPolicy, demo_iranian_policy_pack
+from .profiles import CalculationMode, ComponentRule, PayrollCalculationProfile, RuleReadiness, observed_source_profile
+from .reconciliation_engine import PopulationReconciliation, flatten_diffs, reconcile_population
 from .retro import RetroPeriod, RetroResult, calculate_retroactive
 from .service import PayrollService
+from .snapshot import EffectiveOrder, PayrollSnapshot, latest_effective_order
+from .source_replay import SourceReplay, SourceReplayCalculator, replay_many
 
 __all__ = [
+    "CalculationMode",
+    "ComponentRule",
     "ContributionPolicy",
+    "EffectiveOrder",
+    "EmployeeDiff",
+    "LineDiff",
     "PayrollCalculation",
+    "PayrollCalculationProfile",
     "PayrollCalculator",
     "PayrollLine",
     "PayrollResult",
     "PayrollService",
+    "PayrollSnapshot",
+    "PopulationReconciliation",
     "RetroPeriod",
     "RetroResult",
+    "RuleReadiness",
+    "SourceReplay",
+    "SourceReplayCalculator",
     "TaxBracket",
     "TaxPolicy",
     "calculate_retroactive",
+    "compare_snapshots",
     "demo_iranian_policy_pack",
+    "flatten_diffs",
+    "latest_effective_order",
+    "observed_source_profile",
+    "population_component_totals",
+    "reconcile_population",
+    "replay_many",
 ]
