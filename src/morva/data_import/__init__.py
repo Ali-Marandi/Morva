388764c002116378485d998d36e821d893d1ffc7
‪Ali Marandi‬‏ <Marandi@outlook.com>
2026-09-01 03:22:32 +0200

    fix: route importer to robust service v2

--- src/morva/data_import/__init__.py
+++ src/morva/data_import/__init__.py
@@ -2,6 +2,6 @@
 
 from .models import ImportRecord, ImportReport, ReconciliationException
 from .xlsx_reader import read_xlsx_table
-from .service import MorvaImportService
+from .service_v2 import MorvaImportService
 
 __all__ = ["ImportRecord", "ImportReport", "ReconciliationException", "MorvaImportService", "read_xlsx_table"]
