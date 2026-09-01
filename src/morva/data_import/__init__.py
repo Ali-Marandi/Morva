9f9b56484d934a9dd05b8d5233bec9761558b521
‪Ali Marandi‬‏ <Marandi@outlook.com>
2026-09-01 03:20:27 +0200

    feat: add payroll data import package

---
+++ src/morva/data_import/__init__.py
@@ -0,0 +1,7 @@
+"""Import adapters for Iranian education payroll source reports."""
+
+from .models import ImportRecord, ImportReport, ReconciliationException
+from .xlsx_reader import read_xlsx_table
+from .service import MorvaImportService
+
+__all__ = ["ImportRecord", "ImportReport", "ReconciliationException", "MorvaImportService", "read_xlsx_table"]
