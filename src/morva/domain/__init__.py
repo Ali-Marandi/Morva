47d483bbfcb591de0991e6ee258e7bd9b1d3ac02
‪Ali Marandi‬‏ <Marandi@outlook.com>
2026-09-06 16:47:56 -0500

    feat(core-hr): export profile entities

--- src/morva/domain/__init__.py
+++ src/morva/domain/__init__.py
@@ -1,10 +1,17 @@
+from .core_hr import Dependent, DependentRelationship, Education, EducationLevel, Experience, ExperienceType
 from .models import Employee, EmployeeStatus, Employment, EmploymentType, Money, Person
 
 __all__ = [
+    "Dependent",
+    "DependentRelationship",
+    "Education",
+    "EducationLevel",
     "Employee",
     "EmployeeStatus",
     "Employment",
     "EmploymentType",
+    "Experience",
+    "ExperienceType",
     "Money",
     "Person",
 ]
