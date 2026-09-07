9ce4183592a1f4ec959fdc54d5e8b5a0d7578948
‪Ali Marandi‬‏ <Marandi@outlook.com>
2026-09-06 19:45:20 -0500

    fix: simplify Alembic migration setup

--- alembic/env.py
+++ alembic/env.py
@@ -32,7 +32,7 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    configuration = config.get_section(config.config_file_name, {}) if False else config.get_section(config.config_ini_section, {})
+    configuration = config.get_section(config.config_ini_section, {})
     configuration["sqlalchemy.url"] = get_url()
     connectable = engine_from_config(
         configuration,
