02d3fc1899b16ef57aa4a5bf3b19198ec71e191c
‪Ali Marandi‬‏ <Marandi@outlook.com>
2026-09-09 15:18:29 -0500

    feat(m3.15): export self-service routers

--- src/morva/api/v1/__init__.py
+++ src/morva/api/v1/__init__.py
@@ -1,5 +1,7 @@
+from .cases import router as cases_router
 from .imports import router as imports_router
 from .payroll import router as payroll_router
 from .rules import router as rules_router
+from .self_service import router as self_service_router
 
-__all__ = ["imports_router", "payroll_router", "rules_router"]
+__all__ = ["cases_router", "imports_router", "payroll_router", "rules_router", "self_service_router"]
