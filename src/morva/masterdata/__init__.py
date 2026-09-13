06aa1bc5a1b211cdec2cb6dee657dfeb18bacbda
‪Ali Marandi‬‏ <Marandi@outlook.com>
2026-09-13 15:45:24 -0500

    feat: export master-data drift contract

--- src/morva/masterdata/__init__.py
+++ src/morva/masterdata/__init__.py
@@ -1,3 +1,9 @@
+from morva.masterdata.drift import MasterDataDriftResult, detect_master_data_drift
 from morva.masterdata.readiness import MasterDataReadinessResult, verify_master_data_readiness
 
-__all__ = ["MasterDataReadinessResult", "verify_master_data_readiness"]
+__all__ = [
+    "MasterDataDriftResult",
+    "MasterDataReadinessResult",
+    "detect_master_data_drift",
+    "verify_master_data_readiness",
+]
