29c0d4cb5d425af24fa1b4cbecca453a39b3ff6c
‪Ali Marandi‬‏ <Marandi@outlook.com>
2026-09-26 06:24:22 +0900

    fix(m4.68): remove unused test imports

--- tests/test_independent_historical_m4_67_verification_persistence_m4_68.py
+++ tests/test_independent_historical_m4_67_verification_persistence_m4_68.py
@@ -4,12 +4,6 @@ from datetime import datetime
 
 import pytest
 
-from morva.persistence.historical_m4_63_receipt_history_integrity_m4_64 import (
-    HistoricalM463ReceiptHistoryIntegrityRepository,
-)
-from morva.persistence.independent_historical_m4_61_receipt_history_verification_m4_63 import (
-    IndependentHistoricalM461ReceiptHistoryIntegrityReceiptRecord,
-)
 from morva.persistence.independent_historical_m4_64_verification_receipts_m4_66 import (
     IndependentHistoricalM464ReceiptHistoryIntegrityReceiptRepository,
 )
